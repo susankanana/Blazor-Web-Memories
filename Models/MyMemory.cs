9ba4d624dbace57aac0544e219addd210c3627b8
Susan Kanana <suzzannekans@gmail.com>
2024-01-05 12:58:32 +0300

    initial commit

---
+++ Models/MyMemory.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorMemoriesApplication.Models
+{
+    public class MyMemory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }=string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public string Location { get; set; }=string.Empty;
+        public string Image {  get; set; }=string.Empty;
+    }
+}
